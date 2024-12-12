83a5ed84eb44828d7d65225cd7aa5e0007eadad4
Harry <hwang6@stevens.edu>
2024-12-12 17:42:43 -0500

    fixing up sellers.js

--- routes/sellers.js
+++ routes/sellers.js
@@ -99,57 +99,6 @@ router.route('/signup').post(async (req, res) => {
   }
 });
 
-// /sellers/login
-
-router
-  .route('/')
-  .get(async (req, res) => {
-    try {
-      const sellersList = await sellersData.getAllSellers();
-      return res.render(sellersList);
-    } catch (e) {
-      return res.status(500).render('error', { error: e });
-    }
-  })
-  .post(async (req, res) => {
-    const requestBodyData = req.body;
-
-    if (!requestBodyData || Object.keys(requestBodyData).length === 0) {
-      return res
-        .status(400)
-        .render('error', { error: 'Could not get Request Body.' });
-    }
-    try {
-      // Validation of Request Body Data
-
-      requestBodyData.username = validation.checkString(
-        requestBodyData.username
-      );
-      requestBodyData.password = validation.checkString(
-        requestBodyData.password
-      );
-      requestBodyData.name = validation.checkString(requestBodyData.name);
-      requestBodyData.town = validation.checkString(requestBodyData.town);
-    } catch (e) {
-      return res.status(500).render('error', { error: e });
-    }
-
-    //
-
-    try {
-      const { username, password, name, town } = requestBodyData;
-
-      const newSeller = await sellersData.createSeller(
-        username,
-        password,
-        name,
-        town
-      );
-    } catch (e) {
-      return res.status(500).render('error', { error: e });
-    }
-  });
-
 // ADDLISTING
 
 router.route('/addlisting').get(async (req, res) => {
