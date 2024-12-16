2deaae8628936599ae8dfb4307ec2b18ab963724
Barndude <bwu22@stevens.edu>
2024-12-16 18:01:36 -0500

    hotfix

--- data/index.js
+++ data/index.js
@@ -2,6 +2,6 @@ import { customersDataFunctions } from './customers.js';
 import * as sellerDataFunctions from './seller.js';
 import { orderDataFunctions } from './orders.js';
 
-export const customer = customerDataFunctions;
+export const customer = customersDataFunctions;
 export const sellersData = sellerDataFunctions;
 export const ordersData = orderDataFunctions;
