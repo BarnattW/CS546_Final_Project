1b5f846b79f24dd2ba2047ebad349aa35a677df8
madisaan <mcminime@outlook.com>
2024-12-16 17:22:37 -0500

    seed.js

--- utils/seed.js
+++ utils/seed.js
@@ -2,6 +2,8 @@ import {dbConnection, closeConnection} from '../config/mongoConnection.js';
 import * as customer from "../data/customers.js";
 import * as seller from "../data/seller.js";
 import * as comment from "../data/comments.js";
+import * as review from "../data/reviews.js";
+import * as order from "../data/orders.js";
 
 const db = await dbConnection();
 await db.dropDatabase();
@@ -43,16 +45,70 @@ let listing3 = await seller.createListing(barnattID,
 	"Minimal Wear"
 );
 
+let listing1ID = listing1._id.toString()
+let listing2ID = listing2._id.toString()
+let listing3ID = listing3._id.toString()
 
+let comment1 = await comment.createComment(
+	madisonID,
+	"Madison",
+	listing1ID,
+	"This chair reminds me of Davis Hall."
+);
 
+let comment2 = await comment.createComment(
+	omID,
+	"Om",
+	listing1ID,
+	"Many college dorms have rocking chairs like this."
+);
 
+let comment3 = await comment.createComment(
+	madisonID,
+	"Madison",
+	listing3ID,
+	"I wish this came in pink."
+);
 
+let review1 = review.reviewDataFunctions.createReview(
+	madisonID,
+	"Madison",
+	listing2ID,
+	4,
+	"Love this just wish it was pink."
+);
 
+let review2 = review.reviewDataFunctions.createReview(
+	omID,
+	"Om",
+	listing2ID,
+	5,
+	"Great for getting homework and gaming done!"
+);
 
+let orderItem1 = {
+	itemId: listing1ID,
+	quantity: 1
+};
 
+let order1 = order.orderDataFunctions.createOrder(
+	madisonID,
+	orderItem1,
+	"1 Castle Point Terrace",
+	40
+);
 
+let orderItem2 = {
+	itemId: listing3ID,
+	quantity: 1
+};
 
-
+let order2 = order.orderDataFunctions.createOrder(
+	omID,
+	orderItem2,
+	"1 River Street",
+	80
+);
 
 console.log('Done seeding database');
 
