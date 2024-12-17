53b7eb141d76254f1de0f018f286d9844e27f385
Harry <hwang6@stevens.edu>
2024-12-17 18:39:52 -0500

    test

--- public/js/listings/listingspage.js
+++ public/js/listings/listingspage.js
@@ -2,62 +2,144 @@
  * Listing Form
  */
 
-import { checkInputEmpty, showErrorDialog } from "../helpers.js";
+import { checkInputEmpty, showErrorDialog } from '../helpers.js';
 
 // add to cart
-const itemPageAddToCartForm = document.getElementById("itemAddToCart");
-itemPageAddToCartForm.addEventListener("submit", async (event) => {
-	event.preventDefault();
+const itemPageAddToCartForm = document.getElementById('itemAddToCart');
+itemPageAddToCartForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
 
-	try {
-		const listingId = document.getElementById("addToCartBtn").dataset.listingid;
-		const quantity = document.getElementById(`quantity-${listingId}`);
-		quantity.value = checkInputEmpty(quantity, "Quantity");
-		if (quantity.value < 0) quantity.value = 1;
-		else if (quantity.value > 5) quantity.value = 5;
-		const response = await fetch("/customers/cart", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ listingId, quantity: quantity.value }),
-		});
+  try {
+    const listingId = document.getElementById('addToCartBtn').dataset.listingid;
+    const quantity = document.getElementById(`quantity-${listingId}`);
+    quantity.value = checkInputEmpty(quantity, 'Quantity');
+    if (quantity.value < 0) quantity.value = 1;
+    else if (quantity.value > 5) quantity.value = 5;
+    const response = await fetch('/customers/cart', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ listingId, quantity: quantity.value }),
+    });
 
-		if (response.ok) {
-			window.location.href = "/customers/cart";
-		} else {
-			const data = await response.json();
-			throw JSON.stringify(data.error);
-		}
-	} catch (e) {
-		showErrorDialog(e);
-	}
+    if (response.ok) {
+      window.location.href = '/customers/cart';
+    } else {
+      const data = await response.json();
+      throw JSON.stringify(data.error);
+    }
+  } catch (e) {
+    showErrorDialog(e);
+  }
 });
 
 // add to wishlist
-const addToWishlistBtn = document.getElementById("addToWishlistBtn");
+const addToWishlistBtn = document.getElementById('addToWishlistBtn');
 
-addToWishlistBtn.addEventListener("click", async (e) => {
-	e.preventDefault();
+addToWishlistBtn.addEventListener('click', async (e) => {
+  e.preventDefault();
 
-	try {
-		const listingId = addToWishlistBtn.dataset.listingid;
-		if (!listingId) throw "listingId is missing";
-		const response = await fetch("/customers/wishlist", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ listingId }),
-		});
+  try {
+    const listingId = addToWishlistBtn.dataset.listingid;
+    if (!listingId) throw 'listingId is missing';
+    const response = await fetch('/customers/wishlist', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ listingId }),
+    });
 
-		if (response.ok) {
-			window.location.href = "/customers/wishlist";
-		} else {
-			const data = await response.json();
-			throw JSON.stringify(data.error);
-		}
-	} catch (e) {
-		showErrorDialog(e);
-	}
+    if (response.ok) {
+      window.location.href = '/customers/wishlist';
+    } else {
+      const data = await response.json();
+      throw JSON.stringify(data.error);
+    }
+  } catch (e) {
+    showErrorDialog(e);
+  }
 });
+
+import {
+  checkInputEmpty,
+  checkInputLength,
+  showErrorDialog,
+} from './helpers.js';
+
+// Checks Reviews
+
+const addReviewForm = document.getElementById('addReviewForm');
+if (addReviewForm) {
+  addReviewForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    try {
+      const reviewText = document.getElementById('reviewText');
+      const rating = document.getElementById('rating');
+      reviewText.value = checkInputEmpty(reviewText, 'Review Text');
+      rating.value = checkInputEmpty(rating, 'Rating');
+      if (rating.value < 1 || rating.value > 5)
+        throw `Rating must be between 1 and 5`;
+      const response = await fetch(
+        `/reviews/${addReviewForm.dataset.listingid}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            reviewText: reviewText.value,
+            rating: rating.value,
+          }),
+        }
+      );
+
+      if (response.ok) {
+        location.reload();
+      } else {
+        const data = await response.json();
+        throw JSON.stringify(data.error);
+      }
+    } catch (e) {
+      showErrorDialog(e);
+    }
+  });
+}
+
+// Checks Comments
+
+const addCommentForm = document.getElementById('addCommentForm');
+if (addCommentForm) {
+  addCommentForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    try {
+      const commentText = document.getElementById('commentText');
+      commentText.value = checkInputEmpty(commentText, 'Comment Text');
+      const response = await fetch(
+        `/comments/${addCommentForm.dataset.listingid}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            commentText: commentText.value,
+          }),
+        }
+      );
+
+      if (response.ok) {
+        location.reload();
+      } else {
+        const data = await response.json();
+        throw data.error;
+      }
+    } catch (e) {
+      clientErrorDiv.hidden = false;
+      clientErrorDiv.innerHTML = e;
+    }
+  });
+}
--- public/js/listings/sellerlistingspage.js
+++ public/js/listings/sellerlistingspage.js
@@ -3,208 +3,208 @@
  */
 
 import {
-	checkInputEmpty,
-	checkNumRange,
-	convertImage,
-	showErrorDialog,
-} from "../helpers.js";
+  checkInputEmpty,
+  checkNumRange,
+  convertImage,
+  showErrorDialog,
+} from '../helpers.js';
 
 // Add Listings form
-const addListingForm = document.getElementById("addListingForm");
+const addListingForm = document.getElementById('addListingForm');
 
-const listingName = document.getElementById("listingName");
-const listingDescription = document.getElementById("listingDescription");
-const listingPrice = document.getElementById("listingPrice");
-const listingCategory = document.getElementById("listingSubcategory");
-const listingCondition = document.getElementById("listingCondition");
-const listingImage = document.getElementById("listingImage");
+const listingName = document.getElementById('listingName');
+const listingDescription = document.getElementById('listingDescription');
+const listingPrice = document.getElementById('listingPrice');
+const listingCategory = document.getElementById('listingCategory');
+const listingCondition = document.getElementById('listingCondition');
+const listingImage = document.getElementById('listingImage');
 
-addListingForm.addEventListener("submit", async (event) => {
-	event.preventDefault();
+addListingForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
 
-	try {
-		listingName.value = checkInputEmpty(listingName, "Listing Name");
-		listingDescription.value = checkInputEmpty(
-			listingDescription,
-			"Listing Description"
-		);
-		listingPrice.value = checkInputEmpty(listingPrice, "Listing Price");
-		listingCategory.value = checkInputEmpty(
-			listingCategory,
-			"Listing Category"
-		);
-		listingCondition.value = checkInputEmpty(
-			listingCondition,
-			"Listing Condition"
-		);
+  try {
+    listingName.value = checkInputEmpty(listingName, 'Listing Name');
+    listingDescription.value = checkInputEmpty(
+      listingDescription,
+      'Listing Description'
+    );
+    listingPrice.value = checkInputEmpty(listingPrice, 'Listing Price');
+    listingCategory.value = checkInputEmpty(
+      listingCategory,
+      'Listing Category'
+    );
+    listingCondition.value = checkInputEmpty(
+      listingCondition,
+      'Listing Condition'
+    );
 
-		if (!listingImage.files) throw `An image is required`;
+    if (!listingImage.files) throw `An image is required`;
 
-		const image = await convertImage(listingImage.files[0]);
-		const price = Number(listingPrice.value);
-		checkNumRange(price, 1, 10000);
-		const response = await fetch("/sellers/addlisting", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				itemName: listingName.value,
-				itemDescription: listingDescription.value,
-				itemPrice: price,
-				itemImage: image,
-				itemCategory: listingCategory.value,
-				condition: listingCondition.value,
-			}),
-		});
+    const image = await convertImage(listingImage.files[0]);
+    const price = Number(listingPrice.value);
+    checkNumRange(price, 1, 10000);
+    const response = await fetch('/sellers/addlisting', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        itemName: listingName.value,
+        itemDescription: listingDescription.value,
+        itemPrice: price,
+        itemImage: image,
+        itemCategory: listingCategory.value,
+        condition: listingCondition.value,
+      }),
+    });
 
-		if (!response.ok) {
-			const data = await response.json();
-			throw JSON.stringify(data.error);
-		} else {
-			window.location.reload();
-		}
-	} catch (e) {
-		showErrorDialog(e);
-	}
+    if (!response.ok) {
+      const data = await response.json();
+      throw JSON.stringify(data.error);
+    } else {
+      window.location.reload();
+    }
+  } catch (e) {
+    showErrorDialog(e);
+  }
 });
 
 // Delete listing
-Array.from(document.getElementsByClassName("deleteListingBtn")).forEach((btn) =>
-	btn.addEventListener("click", async (e) => {
-		const row = e.target.closest("tr");
-		const listingId = row.dataset.listingid;
+Array.from(document.getElementsByClassName('deleteListingBtn')).forEach((btn) =>
+  btn.addEventListener('click', async (e) => {
+    const row = e.target.closest('tr');
+    const listingId = row.dataset.listingid;
 
-		try {
-			const response = await fetch(`/sellers/listings/${listingId}`, {
-				method: "DELETE",
-			});
-			if (response.ok) {
-				window.location.reload();
-			} else {
-				throw `Could not delete listing with ID ${listingId}`;
-			}
-		} catch (e) {
-			showErrorDialog(e);
-		}
-	})
+    try {
+      const response = await fetch(`/sellers/listings/${listingId}`, {
+        method: 'DELETE',
+      });
+      if (response.ok) {
+        window.location.reload();
+      } else {
+        throw `Could not delete listing with ID ${listingId}`;
+      }
+    } catch (e) {
+      showErrorDialog(e);
+    }
+  })
 );
 
 // Modals
-const createListingModal = document.getElementById("createListingModal");
-const openCreateListingBtn = document.getElementById("openCreateListingBtn");
-const closeCreateListingBtn = document.getElementById("closeCreateListingBtn");
+const createListingModal = document.getElementById('createListingModal');
+const openCreateListingBtn = document.getElementById('openCreateListingBtn');
+const closeCreateListingBtn = document.getElementById('closeCreateListingBtn');
 
-openCreateListingBtn.addEventListener("click", () => {
-	addListingForm.reset();
+openCreateListingBtn.addEventListener('click', () => {
+  addListingForm.reset();
 
-	createListingModal.classList.remove("hidden");
-	createListingModal.classList.add("flex");
+  createListingModal.classList.remove('hidden');
+  createListingModal.classList.add('flex');
 });
 
-closeCreateListingBtn.addEventListener("click", () => {
-	createListingModal.classList.add("hidden");
+closeCreateListingBtn.addEventListener('click', () => {
+  createListingModal.classList.add('hidden');
 });
 
 // Edit Listing
 
-const editListingModal = document.getElementById("editListingModal");
-const closeEditListingBtn = document.getElementById("closeEditListingBtn");
+const editListingModal = document.getElementById('editListingModal');
+const closeEditListingBtn = document.getElementById('closeEditListingBtn');
 
-Array.from(document.getElementsByClassName("editListingBtn")).forEach((btn) =>
-	btn.addEventListener("click", (e) => {
-		e.preventDefault();
-		// open modal
-		editListingModal.classList.remove("hidden");
-		editListingModal.classList.add("flex");
+Array.from(document.getElementsByClassName('editListingBtn')).forEach((btn) =>
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    // open modal
+    editListingModal.classList.remove('hidden');
+    editListingModal.classList.add('flex');
 
-		const row = e.target.closest("tr");
-		const listingId = row.dataset.listingid;
-		// add hidden listingId
-		document.getElementById("editListingForm").dataset.listingid = listingId;
+    const row = e.target.closest('tr');
+    const listingId = row.dataset.listingid;
+    // add hidden listingId
+    document.getElementById('editListingForm').dataset.listingid = listingId;
 
-		const itemName = row.querySelector("td:nth-child(2)").innerText;
-		const itemDescription = row.querySelector("td:nth-child(3)").innerText;
-		const itemPrice = row
-			.querySelector("td:nth-child(6)")
-			.innerText.replace("$", "");
-		const itemCategory = row.querySelector("td:nth-child(4)").innerText;
-		const itemCondition = row.querySelector("td:nth-child(5)").innerText;
+    const itemName = row.querySelector('td:nth-child(2)').innerText;
+    const itemDescription = row.querySelector('td:nth-child(3)').innerText;
+    const itemPrice = row
+      .querySelector('td:nth-child(6)')
+      .innerText.replace('$', '');
+    const itemCategory = row.querySelector('td:nth-child(4)').innerText;
+    const itemCondition = row.querySelector('td:nth-child(5)').innerText;
 
-		document.getElementById("editListingName").value = itemName;
-		document.getElementById("editListingDescription").value = itemDescription;
-		document.getElementById("editListingPrice").value =
-			parseFloat(itemPrice).toFixed(2);
-		document.getElementById("editListingCategory").value = itemCategory;
-		document.getElementById("editListingCondition").value = itemCondition;
-	})
+    document.getElementById('editListingName').value = itemName;
+    document.getElementById('editListingDescription').value = itemDescription;
+    document.getElementById('editListingPrice').value =
+      parseFloat(itemPrice).toFixed(2);
+    document.getElementById('editListingCategory').value = itemCategory;
+    document.getElementById('editListingCondition').value = itemCondition;
+  })
 );
 
-closeEditListingBtn.addEventListener("click", () => {
-	editListingModal.classList.add("hidden");
+closeEditListingBtn.addEventListener('click', () => {
+  editListingModal.classList.add('hidden');
 });
 
-const editListingForm = document.getElementById("editListingForm");
+const editListingForm = document.getElementById('editListingForm');
 
-editListingForm.addEventListener("submit", async (event) => {
-	event.preventDefault();
+editListingForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
 
-	try {
-		const listingId = editListingForm.dataset.listingid;
-		const listingName = document.getElementById("editListingName");
-		const listingDescription = document.getElementById(
-			"editListingDescription"
-		);
-		const listingPrice = document.getElementById("editListingPrice");
-		const listingCategory = document.getElementById("editListingCategory");
-		const listingCondition = document.getElementById("editListingCondition");
+  try {
+    const listingId = editListingForm.dataset.listingid;
+    const listingName = document.getElementById('editListingName');
+    const listingDescription = document.getElementById(
+      'editListingDescription'
+    );
+    const listingPrice = document.getElementById('editListingPrice');
+    const listingCategory = document.getElementById('editListingCategory');
+    const listingCondition = document.getElementById('editListingCondition');
 
-		if (!listingId) throw `Listing ID not found`;
-		listingName.value = checkInputEmpty(listingName, "Listing Name");
-		listingDescription.value = checkInputEmpty(
-			listingDescription,
-			"Listing Description"
-		);
-		listingPrice.value = checkInputEmpty(listingPrice, "Listing Price");
-		const price = Number(listingPrice.value);
-		checkNumRange(price, 1, 10000);
-		listingCategory.value = checkInputEmpty(
-			listingCategory,
-			"Listing Category"
-		);
-		listingCondition.value = checkInputEmpty(
-			listingCondition,
-			"Listing Condition"
-		);
+    if (!listingId) throw `Listing ID not found`;
+    listingName.value = checkInputEmpty(listingName, 'Listing Name');
+    listingDescription.value = checkInputEmpty(
+      listingDescription,
+      'Listing Description'
+    );
+    listingPrice.value = checkInputEmpty(listingPrice, 'Listing Price');
+    const price = Number(listingPrice.value);
+    checkNumRange(price, 1, 10000);
+    listingCategory.value = checkInputEmpty(
+      listingCategory,
+      'Listing Category'
+    );
+    listingCondition.value = checkInputEmpty(
+      listingCondition,
+      'Listing Condition'
+    );
 
-		let image;
-		// get image url
-		if (editListingImage.files.length === 0) {
-			const row = document.querySelector(`tr[data-listingid="${listingId}"]`);
-			image = row.querySelector("td:nth-child(1) img").src;
-		} else image = await convertImage(editListingImage.files[0]);
-		const response = await fetch(`/sellers/listings/${listingId}`, {
-			method: "PUT",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				itemName: listingName.value,
-				itemDescription: listingDescription.value,
-				itemPrice: price,
-				itemImage: image,
-				itemCategory: listingCategory.value,
-				condition: listingCondition.value,
-			}),
-		});
+    let image;
+    // get image url
+    if (editListingImage.files.length === 0) {
+      const row = document.querySelector(`tr[data-listingid="${listingId}"]`);
+      image = row.querySelector('td:nth-child(1) img').src;
+    } else image = await convertImage(editListingImage.files[0]);
+    const response = await fetch(`/sellers/listings/${listingId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        itemName: listingName.value,
+        itemDescription: listingDescription.value,
+        itemPrice: price,
+        itemImage: image,
+        itemCategory: listingCategory.value,
+        condition: listingCondition.value,
+      }),
+    });
 
-		if (!response.ok) {
-			const data = await response.json();
-			throw JSON.stringify(data.error);
-		} else {
-			window.location.reload();
-		}
-	} catch (e) {
-		showErrorDialog(e);
-	}
+    if (!response.ok) {
+      const data = await response.json();
+      throw JSON.stringify(data.error);
+    } else {
+      window.location.reload();
+    }
+  } catch (e) {
+    showErrorDialog(e);
+  }
 });
